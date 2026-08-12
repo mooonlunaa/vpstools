b8c7ceddb93c1b4e72c66b27f564906623019bbc
Moon Luna <cakraganzzx@gmail.com>
2026-08-12 20:40:14 +0700

    Update and rename ranzx.sh to vpstools.sh

--- vpstools.sh
+++ vpstools.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-# VPS Tools Dashboard v2.1 (Nia edition ✧)
-# Cek spesifikasi & install tools di VPS
-
 # ── Colors ──────────────────────────────────────────────
 RED='\033[0;31m'
 GREEN='\033[0;32m'
